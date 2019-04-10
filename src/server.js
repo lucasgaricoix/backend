45af765db1f3b7a8c2d8b106555ed286e08e1e0a
Lucas Garicoix <lucasgaricoix@hotmail.com>
2019-04-10 01:08:46 -0300

    Criando o backend

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors = require('cors');
+
+app.use(cors());
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
+
+mongoose.connect('mongodb+srv://omnistack:123@cluster0-bcdam.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true
+})
+
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+
+app.use(require('./routes'));
+
+server.listen(3003);
