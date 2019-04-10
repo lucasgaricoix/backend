31d3d3fcea88ae2b9f52d20d405d2a1ed4778371
Lucas Garicoix <lucasgaricoix@hotmail.com>
2019-04-10 01:16:38 -0300

    adiciona variaveis de ambiente

--- src/models/File.js
+++ src/models/File.js
@@ -17,6 +17,7 @@ const File = new mongoose.Schema({
 });
 
 File.virtual('url').get(function() {
+    const url = process.env.URL || 'http://localhost:3003'
     return `http://localhost:3003/files/${encodeURIComponent(this.path)}`;
 })
 
